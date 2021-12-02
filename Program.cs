78a9d526b164d48b4cc08622beba78e1eca2cc1a
Ashwinivaidya <95351336+Ashwinivaidya@users.noreply.github.com>
2021-12-02 11:16:11 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
 
-namespace array
+namespace ConsoleApp16
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] num = { 600, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            Console.WriteLine("origanl array element");
-            for(int i = 0; i < num.Length; i++)
-            {
-                Console.WriteLine(num[i] + "");
-
-            }
-            Console.WriteLine("\ncheck the number of the array are consective or not"!+test(num));
-
-        }
-        public static bool test(int[]num)
-        {
-            Array.Sort(num);
-            for (int i = 1; i < num.Length; i++)
-                if (num[i] - num[i - 1] != 1)
-                    return false;
-            return true;
+            string mystr, rev;
+            mystr = "ashu";
+            rev = "";
+            Console.WriteLine("my srting is  {o}", mystr);
+            int len;
+            len = mystr.Length-1;
+            while(len>=0)
 
             {
+                rev = rev + mystr[len];
+                len--;
 
             }
+            Console.WriteLine("my srting is  {o}", rev);
 
         }
     }
-         
 }
