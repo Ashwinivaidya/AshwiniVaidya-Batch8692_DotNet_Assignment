b002240413357c477654ac5a3c0c1873211bca54
Ashwinivaidya <95351336+Ashwinivaidya@users.noreply.github.com>
2021-12-07 12:09:41 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 
-namespace ConsoleApp16
+namespace Find_maximum_and_minimum_element_in_an_array
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            string mystr, rev;
-            mystr = "ashu";
-            rev = "";
-            Console.WriteLine("my srting is  {o}", mystr);
-            int len;
-            len = mystr.Length-1;
-            while(len>=0)
+            int[] arr1 = new int[100];
+            int i, mx, mn, n;
 
+
+            Console.Write("\n\nFind maximum and minimum element in an array :\n");
+            Console.Write("--------------------------------------------------\n");
+
+            Console.Write("Input the number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
             {
-                rev = rev + mystr[len];
-                len--;
-
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("my srting is  {o}", rev);
 
+
+            mx = arr1[0];
+            mn = arr1[0];
+
+            for (i = 1; i < n; i++)
+            {
+                if (arr1[i] > mx)
+                {
+                    mx = arr1[i];
+                }
+
+
+                if (arr1[i] < mn)
+                {
+                    mn = arr1[i];
+                }
+            }
+            Console.Write("Maximum element is : {0}\n", mx);
+            Console.Write("Minimum element is : {0}\n\n", mn);
         }
     }
 }
+
